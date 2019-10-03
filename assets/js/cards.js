def03e37a76c161bd284075023a5c5c27481489c
Nayla <nayla.mar2@gmail.com>
2019-10-03 10:48:09 -0300

    Push
    Banderas nuevas

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,10 +1,74 @@
 var cards=[
     {
-        "name": "Brasil",
-        "country": "BRA",
+        "name": "Alemania",
+        "country": "GER",
     },
     {
         "name": "Australia",
         "country": "AUS",
-    }
+    },
+    {
+        "name": "Italia",
+        "country": "ITA", 
+    },
+    {
+        "name": "Holanda",
+        "country": "NED",
+    },
+    {
+        "name": "New Zeland",
+        "country": "NEW",
+    },
+    {
+        "name": "Norway",
+        "country": "NOR",
+    },
+    {
+        "name": "Denmark",
+        "country": "DEN",
+    },
+    {
+        "name": "Ireland",
+        "country": "IRE",
+    },
+    {
+        "name": "China",
+        "country": "CHI",
+    },
+    {
+        "name": "Colombia",
+        "country": "COL",
+    },
+    {
+        "name": "United States",
+        "country": "USA",
+    },
+    {
+        "name": "Japan",
+        "country": "JAP",
+    },
+    {
+        "name": "France",
+        "country": "FRA",
+    },
+    {
+        "name": "Argentina",
+        "country": "ARG",
+    },
+    {
+        "name": "Ukraine",
+        "country": "UKR",
+    },
+    {
+        "name": "United Kingdom",
+        "country": "UKB",
+    },
+    {
+        "name": "Grecia",
+        "country": "GRE",
+    },
+    {
+        "name": "Turkey",
+        "country": "TUR",
+    },
 ];
