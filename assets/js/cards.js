ab00d44e7127604828cdbf18fab5ffd0aa0f43e0
Nayla <nayla.mar2@gmail.com>
2019-10-05 01:48:13 -0300

    Cambio en la animación
    -Animación de Vertical (ejeY) a horizontal (ejeX)
    -Agregado del valor "call" en el archivo cards.js

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -2,30 +2,37 @@ var cards=[
     {
         "name": "Alemania",
         "country": "GER",
+        "call" : "0",
     },
     {
         "name": "Australia",
         "country": "AUS",
+        "call" : "0",
     },
     {
         "name": "Italia",
-        "country": "ITA", 
+        "country": "ITA",
+        "call" : "0",
     },
     {
         "name": "Holanda",
         "country": "NED",
+        "call" : "0",
     },
     {
         "name": "New Zeland",
         "country": "NEW",
+        "call" : "0",
     },
     {
         "name": "Norway",
         "country": "NOR",
+        "call" : "0",
     },
     {
         "name": "Denmark",
         "country": "DEN",
+        "call" : "0",
     },
     {
         "name": "Ireland",
@@ -35,41 +42,51 @@ var cards=[
     {
         "name": "China",
         "country": "CHI",
+        "call" : "0",
     },
     {
         "name": "Colombia",
         "country": "COL",
+        "call" : "0",
     },
     {
         "name": "United States",
         "country": "USA",
+        "call" : "0",
     },
     {
         "name": "Japan",
         "country": "JAP",
+        "call" : "0",
     },
     {
         "name": "France",
         "country": "FRA",
+        "call" : "0",
     },
     {
         "name": "Argentina",
         "country": "ARG",
+        "call" : "0",
     },
     {
         "name": "Ukraine",
         "country": "UKR",
+        "call" : "0",
     },
     {
         "name": "United Kingdom",
         "country": "UKB",
+        "call" : "0",
     },
     {
         "name": "Grecia",
         "country": "GRE",
+        "call" : "0",
     },
     {
         "name": "Turkey",
         "country": "TUR",
+        "call" : "0",
     },
 ];
